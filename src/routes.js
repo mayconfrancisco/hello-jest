be31d0de06f6ada2c749f84c0e9183df7fb67c1d
Maycon Francisco <mayconjf@gmail.com>
2019-10-29 11:29:43 -0300

    first test passed

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,8 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
 
+routes.post('/users', (req, resp) => {
+  return resp.json({ id: 1 });
+});
+
 export default routes;
