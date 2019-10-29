554cad948cb99ec5f44a3aa0a16bb38b7f97c502
Maycon Francisco <mayconjf@gmail.com>
2019-10-29 11:55:41 -0300

    added duplicate registration test

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,17 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  // beforeAll() executa uma só vez antes de executar todos os testes
+  // beforeEach() executa uma vez a cada teste
+  // afterAll()
+  // afterEach()
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +22,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Maycon Francisco',
+        email: 'maycon@francisco.com',
+        password_hash: '123123',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Maycon Francisco',
+        email: 'maycon@francisco.com',
+        password_hash: '123123',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,13 @@ import User from '../models/User';
 
 class UserController {
   async store(req, resp) {
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+    if (checkEmail) {
+      return resp.status(400).json({ error: 'Duplicated email' });
+    }
+
     const user = await User.create(req.body);
 
     return resp.json(user);
